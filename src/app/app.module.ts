dd2ab1c43079d46d7be91721425eecbb9550e569
PabloRodriguez <rodherp3644@gmail.com>
2023-01-12 11:45:11 +0000

    Componentes añadidos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './componentes/welcome/welcome.component';
+import { ApiResponseComponent } from './componentes/api-response/api-response.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ApiResponseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/api-response/api-response.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-api-response',
+  templateUrl: './api-response.component.html',
+  styleUrls: ['./api-response.component.scss']
+})
+export class ApiResponseComponent {
+
+}
